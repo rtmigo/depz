b48cc07684398605d850f8d86d525a8316fa6c30
rtmigo <art.galkin@gmail.com>
2021-03-05 01:48:08 +0300

    version

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setup(
   # https://pypi.org/classifiers/
   classifiers=[
     # "Development Status :: 4 - Beta",
-    "Development Status :: 2 - Pre-Alpha",
-    # "Development Status :: 3 - Alpha",
+    # "Development Status :: 2 - Pre-Alpha",
+    "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
     'License :: OSI Approved :: BSD License',
     "Programming Language :: Python :: 3.7",
